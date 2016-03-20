8f26dbdf301f1d6d14bf7b5f9ef0776c28230f68
OHTA Shogo <athos0220@gmail.com>
2016-03-20 21:53:17 +0900

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-with-env-vars "0.1.0-SNAPSHOT"
+(defproject lein-with-env-vars "0.1.0"
   :description "A Leiningen plugin for performing a task with environment variable settings loaded from project.clj"
   :url "https://github.com/athos/lein-with-env-vars"
   :license {:name "Eclipse Public License"
